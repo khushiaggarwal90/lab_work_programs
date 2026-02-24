820bd157e38a62fcca2d48c261d19bed000ad413
KHUSHI AGGARWAL <khushiaggarwal311@gmail.com>
2026-02-24 11:14:04 +0530

    Add files via upload

---
+++ problem4.py
@@ -0,0 +1,13 @@
+#calculate simple interest.
+# Given values
+principal = 10000  # Principal amount in rupees
+rate = 5            # Annual interest rate in percent
+time = 3            # Time in years
+# Simple Interest formula: SI = (P * R * T)/100
+simple_interest = (principal * rate * time) / 100
+print("Problem 4 - Simple Interest is:", simple_interest)
+
+
+
+#output:
+simple_interest is 1500.0
